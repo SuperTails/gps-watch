378156a7ea0ad7e48b46d93dd69aa9b4576ce445
Gary B <gary@gary600.xyz>
2024-02-08 11:19:41 -0500

    Initial commit forked from knurling-rs/app-template

---
+++ src/bin/minimal.rs
@@ -0,0 +1,70 @@
+#![no_main]
+#![no_std]
+#![feature(type_alias_impl_trait)]
+
+use test_app as _; // global logger + panicking-behavior + memory layout
+
+// TODO(7) Configure the `rtic::app` macro
+#[rtic::app(
+    // TODO: Replace `some_hal::pac` with the path to the PAC
+    device = stm32l4xx_hal::pac,
+    // TODO: Replace the `FreeInterrupt1, ...` with free interrupt vectors if software tasks are used
+    // You can usually find the names of the interrupt vectors in the some_hal::pac::interrupt enum.
+    dispatchers = [EXTI0]
+)]
+mod app {
+    use stm32l4xx_hal as hal;
+
+    use hal::{pac, prelude::*};
+
+    // Shared resources go here
+    #[shared]
+    struct Shared {
+        // TODO: Add resources
+    }
+
+    // Local resources go here
+    #[local]
+    struct Local {
+        // TODO: Add resources
+    }
+
+    #[init]
+    fn init(_cx: init::Context) -> (Shared, Local) {
+        let peripherals = pac::Peripherals::take();
+
+        defmt::info!("init");
+
+        // TODO setup monotonic if used
+        // let sysclk = { /* clock setup + returning sysclk as an u32 */ };
+        // let token = rtic_monotonics::create_systick_token!();
+        // rtic_monotonics::systick::Systick::new(cx.core.SYST, sysclk, token);
+
+        blink_led::spawn().ok();
+
+        (
+            Shared {
+                // Initialization of shared resources go here
+            },
+            Local {
+                // Initialization of local resources go here
+            },
+        )
+    }
+
+    // Optional idle, can be removed if not needed.
+    #[idle]
+    fn idle(_: idle::Context) -> ! {
+        defmt::info!("idle");
+
+        loop {
+            continue;
+        }
+    }
+
+    // TODO: Add tasks
+    #[task(priority = 1)]
+    async fn blink_led(_cx: blink_led::Context) {
+        defmt::info!("Hello from task1!");
+    }
+}
